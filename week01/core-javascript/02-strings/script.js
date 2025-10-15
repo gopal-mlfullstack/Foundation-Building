cf40644e2db1edf96e0986b02e230bab9952a60c
Gopal Mahato <gopalmahatomain2026@gmail.com>
2025-10-15 17:46:49 +0530

    Added some strings method and a practice question at the file source Traversy Media

---
+++ week01/core-javascript/02-strings/script.js
@@ -0,0 +1,17 @@
+// Strings
+let name = "Gopal Mahato";
+console.log(name, typeof name);
+
+console.log(name.length); // get the length of name string
+console.log(name.toUpperCase()); // converted name as all uppercase
+console.log(name.toLowerCase()); // converted name as all lowercase
+console.log(name[3]); // access value index number 3
+console.log(name.indexOf("G"));
+console.log(name.slice(0, 5));
+console.log(name.replace("Gopal Mahato", "GOPAL MAHATO"));
+console.log(name.includes("G"));
+console.log(name);
+console.log(name.split(" "));
+const my_string = "developer";
+const capitalized = my_string.charAt(0).toUpperCase() + my_string.slice(1);
+console.log(capitalized);
